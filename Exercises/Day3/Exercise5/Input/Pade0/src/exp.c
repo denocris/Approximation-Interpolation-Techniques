b2663bf6b4d15d9c615c299e0b039c2c0b11a806
denocris <denobili16@gmail.com>
2017-04-21 14:13:18 +0200

    change range

--- Exercises/Day3/Exercise5/Input/Pade0/src/exp.c
+++ Exercises/Day3/Exercise5/Input/Pade0/src/exp.c
@@ -123,12 +123,12 @@ double adv_pad_exp(double x){
    const double fpart = 1. + 2.*px/(qx - px);
 
    if (iy >= 0) {
-     printf("%f\n",pow(2,fy) - fpart);
-     return fpart * (2 << iy);
+     //printf("%f\n",pow(2,fy) - fpart);
+     return fpart * (1 << iy);
     }
    else{
-     printf("%f\n",pow(2,fy) - fpart);
-     return fpart * 1. / (2 << -1*iy);
+     //printf("%f\n",pow(2,fy) - fpart);
+     return fpart * 1. / (1 << -1*iy);
   }
 
   }
